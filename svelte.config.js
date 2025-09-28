0c24d18de10edcd5ff2d7e9046684725bcf7b2dd
kRxZy_kRxZy <krxzy_krxzy@noreply.codeberg.org>
2025-09-28 17:52:12 +0200

    Fix prerender

--- svelte.config.js
+++ svelte.config.js
@@ -11,15 +11,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		prerender: true,
 		adapter: adapter(),
 		outDir: 'build',
 		alias: {
 			$lib: 'src/lib',
 			$stores: 'src/stores'
 		},
-		prerender: {
-			default: true
-		}
 	},
 	extensions: ['.svelte', '.svx']
 };
