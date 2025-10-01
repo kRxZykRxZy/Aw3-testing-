f15c711a8983ac7f08e7ec41ced90f13812ee30a
kRxZy_kRxZy <krxzy_krxzy@noreply.codeberg.org>
2025-10-01 18:23:47 +0200

    Fix login

--- src/routes/internalapi/login/+server.ts
+++ src/routes/internalapi/login/+server.ts
@@ -1,5 +1,6 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { db } from '$lib/server/db';
+import { generateSessionToken } from '$lib/server/auth';
 import bcrypt from 'bcrypt';
 
 export const POST: RequestHandler = async ({ request }) => {
@@ -10,5 +11,7 @@ export const POST: RequestHandler = async ({ request }) => {
         if (!user || !(await bcrypt.compare(password, user.password_hash)))
             return new Response(JSON.stringify({ error: 'Invalid credentials' }), { status: 401 });
 
-        return new Response(JSON.stringify({ message: 'Login successful', user: { id: user.id, username: user.username } }), { status: 200 });
+        const newExpiry = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+        const ssid = generateSessionToken();
+        return new Response(JSON.stringify({ message: 'Login successful', apiToken: ssid, user: { id: user.id, username: user.username } }), { status: 200 });
 };
